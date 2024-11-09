300c548f91f8a68e5973076c4a532e5c4adb4776
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-09 14:35:21 +0100

    improvement: use template literal

--- exercice 3/script.js
+++ exercice 3/script.js
@@ -8,31 +8,15 @@ let historiqueOperations = []
 
 // Dépôt
 
-console.log(
-    "Vous avez deposé "
-    + depot
-    + " euros. Nouveau solde:"
-    + (solde += depot) 
-    + " euros."
-)
-
-historiqueOperations[historiqueOperations.length]="Dépôt de "+depot+" euros"
-
+console.log(`Vous avez deposé ${depot} euros. Nouveau solde: ${solde += depot} euros.`)
+historiqueOperations[historiqueOperations.length]=`Dépôt de ${depot} euros`
 console.table(historiqueOperations)
 
 // Retrait
 
 if(solde - retrait > 0){
-    console.log(
-        "Vous avez retiré "
-    +   retrait
-    +   " euros. Nouveau solde : "
-    +   (solde -= retrait) 
-    +   " euros."
-    )
-
-    historiqueOperations[historiqueOperations.length]="Retrait de "+retrait+" euros"
-
+    console.log(`Vous avez retiré ${retrait} euros. Nouveau solde : ${solde -= retrait} euros.`)
+    historiqueOperations[historiqueOperations.length]=`Retrait de ${retrait} euros`
     console.table(historiqueOperations)
 } else {
     console.log(
@@ -42,49 +26,25 @@ if(solde - retrait > 0){
 
 // Calcul des intérêts
 
-console.log(
-    "Intérêts annuels de 3% ajoutés. Nouveau solde : "
-    + (solde += solde*TAUX_INTERET)
-    +" euros."
-)
+console.log(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${solde += solde*TAUX_INTERET} euros.`)
 
 //Simulation de plusieurs opérations
 
 let nouveauDepot = 500
 
-console.log(
-    "Vous avez deposé "
-    + nouveauDepot
-    + " euros. Nouveau solde:"
-    + (solde += nouveauDepot) 
-    + " euros."
-)
-
-historiqueOperations[historiqueOperations.length]="Dépôt de "+nouveauDepot+" euros"
-
+console.log(`Vous avez deposé ${nouveauDepot} euros. Nouveau solde: ${solde += nouveauDepot} euros.`)
+historiqueOperations[historiqueOperations.length]=`Dépôt de ${nouveauDepot} euros`
 console.table(historiqueOperations)
 
 let nouveauRetrait = 800
 
 if(solde - nouveauRetrait > 0){
-    console.log(
-        "Vous avez retiré "
-    +   nouveauRetrait
-    +   " euros. Nouveau solde : "
-    +   (solde -= nouveauRetrait) 
-    +   " euros."
-    )
-
-    historiqueOperations[historiqueOperations.length]="Retrait de "+nouveauRetrait+" euros"
+    console.log(`Vous avez retiré ${nouveauRetrait} euros. Nouveau solde : ${solde -= nouveauRetrait} euros.`)
+    historiqueOperations[historiqueOperations.length]=`Retrait de ${nouveauRetrait} euros`
     console.table(historiqueOperations)
+
 } else {
-    console.log(
-        "Solde insuffisant pour effectuer ce retrait."
-    )
+    console.log("Solde insuffisant pour effectuer ce retrait.")
 }
 
-console.log(
-    "Intérêts annuels de 3% ajoutés. Nouveau solde : "
-    + (solde += solde*TAUX_INTERET)
-    +" euros."
-)
+console.log(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${solde += solde*TAUX_INTERET} euros.`)
