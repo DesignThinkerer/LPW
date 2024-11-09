2692a6dfd48a7bbb2cd6fabb51a9ef59d3a6b70b
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-09 14:40:44 +0100

    add timestamps

--- exercice 3/script.js
+++ exercice 3/script.js
@@ -1,6 +1,9 @@
 // Initialisations variables
 
 const TAUX_INTERET = 0.03
+const timestamp = () => {
+    return new Date().toLocaleString()
+}
 let solde = 1000
 let depot = 200
 let retrait = 150
@@ -9,14 +12,14 @@ let historiqueOperations = []
 // Dépôt
 
 console.log(`Vous avez deposé ${depot} euros. Nouveau solde: ${solde += depot} euros.`)
-historiqueOperations[historiqueOperations.length]=`Dépôt de ${depot} euros`
+historiqueOperations[historiqueOperations.length]=`${timestamp()} : Dépôt de ${depot} euros`
 console.table(historiqueOperations)
 
 // Retrait
 
 if(solde - retrait > 0){
     console.log(`Vous avez retiré ${retrait} euros. Nouveau solde : ${solde -= retrait} euros.`)
-    historiqueOperations[historiqueOperations.length]=`Retrait de ${retrait} euros`
+    historiqueOperations[historiqueOperations.length]=`${timestamp()} : Retrait de ${retrait} euros`
     console.table(historiqueOperations)
 } else {
     console.log(
@@ -33,14 +36,14 @@ console.log(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${solde += solde
 let nouveauDepot = 500
 
 console.log(`Vous avez deposé ${nouveauDepot} euros. Nouveau solde: ${solde += nouveauDepot} euros.`)
-historiqueOperations[historiqueOperations.length]=`Dépôt de ${nouveauDepot} euros`
+historiqueOperations[historiqueOperations.length]=`${timestamp()} : Dépôt de ${nouveauDepot} euros`
 console.table(historiqueOperations)
 
 let nouveauRetrait = 800
 
 if(solde - nouveauRetrait > 0){
     console.log(`Vous avez retiré ${nouveauRetrait} euros. Nouveau solde : ${solde -= nouveauRetrait} euros.`)
-    historiqueOperations[historiqueOperations.length]=`Retrait de ${nouveauRetrait} euros`
+    historiqueOperations[historiqueOperations.length]=`${timestamp()} : Retrait de ${nouveauRetrait} euros`
     console.table(historiqueOperations)
 
 } else {
