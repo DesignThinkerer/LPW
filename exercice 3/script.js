6af0277433a594ae6339c2adffdd96777e3d858e
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-09 15:49:30 +0100

    clean up values display

--- exercice 3/script.js
+++ exercice 3/script.js
@@ -14,15 +14,15 @@ const timestamp = () => {
 
 const operationDepot = (DEPOT) => {
     solde += DEPOT
+    historiqueOperations.push(`${timestamp()} : Dépôt de ${Math.abs(DEPOT)} euros`)
     console.info(`Vous avez déposé ${DEPOT} euros. Nouveau solde: ${solde} euros.`)
-    historiqueOperations.push(`${timestamp()} : Dépôt de ${DEPOT} euros`)
     console.table(historiqueOperations)
 }
 
 const operationRetrait = (RETRAIT) => {
     if (solde + RETRAIT >= 0) {
         solde += RETRAIT
-        historiqueOperations.push(`${timestamp()} : Retrait de ${RETRAIT} euros`)
+        historiqueOperations.push(`${timestamp()} : Retrait de ${Math.abs(RETRAIT)} euros`)
         console.info(`Vous avez retiré ${RETRAIT} euros. Nouveau solde: ${solde} euros.`)
         console.table(historiqueOperations)
     } else {
@@ -31,16 +31,17 @@ const operationRetrait = (RETRAIT) => {
 }
 
 const operationSolde = (OPERATION) => {
-    if (OPERATION > 0) {
+    if (Number(OPERATION) > 0) {
         operationDepot(OPERATION)
         return 
     } 
     
-    if (OPERATION < 0) {
+    if (Number(OPERATION) < 0) {
         operationRetrait(OPERATION)
         return
     }
     
+    // Si l'opération est égale à 0 ou qu'il ne s'agit pas d'un nombre
     console.log("L'opération n'est pas valide !")
 }
 
