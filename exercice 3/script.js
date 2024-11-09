64b0d492cf2f8489c32f7c06b3ec85db92a323f2
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-09 15:10:21 +0100

    combine depot + retrait

--- exercice 3/script.js
+++ exercice 3/script.js
@@ -1,65 +1,60 @@
 // Initialisations variables
+const TAUX_INTERET = 0.03;
 
-const TAUX_INTERET = 0.03
-
-let solde = 1000
-let depot = 200
-let retrait = 150
-let historiqueOperations = []
+let solde = 1000;
+let depot = 200;
+let retrait = -150;
+let historiqueOperations = [];
 
 // Utilities
-
 const timestamp = () => {
-    return new Date().toLocaleString()
+    return new Date().toLocaleString();
 }
 
 const operationDepot = (depot, historiqueOperations) => {
-    console.info(`Vous avez deposé ${depot} euros. Nouveau solde: ${solde += depot} euros.`)
-    historiqueOperations[historiqueOperations.length] = `${timestamp()} : Dépôt de ${depot} euros`;
+    solde += depot;
+    console.info(`Vous avez déposé ${depot} euros. Nouveau solde: ${solde} euros.`);
+    historiqueOperations.push(`${timestamp()} : Dépôt de ${depot} euros`);
     console.table(historiqueOperations);
 }
 
 const operationRetrait = (retrait, historiqueOperations) => {
-    console.info(`Vous avez retiré ${retrait} euros. Nouveau solde: ${solde -= retrait} euros.`)
-    historiqueOperations[historiqueOperations.length] = `${timestamp()} : Retrait de ${retrait} euros`;
-    console.table(historiqueOperations);
+    if (solde + retrait >= 0) {
+        solde += retrait;
+        historiqueOperations.push(`${timestamp()} : Retrait de ${retrait} euros`);
+        console.info(`Vous avez retiré ${retrait} euros. Nouveau solde: ${solde} euros.`);
+        console.table(historiqueOperations);
+    } else {
+        console.log("Solde insuffisant pour effectuer ce retrait.");
+    }
 }
 
-const calculInteret = (solde) => {
-    console.log(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${solde += solde*TAUX_INTERET} euros.`)
+const operationSolde = (operation, historiqueOperations) => {
+    if (operation > 0) {
+        operationDepot(operation, historiqueOperations);
+    } else if (operation < 0) {
+        operationRetrait(operation, historiqueOperations);
+    } else {
+        console.log("L'opération n'est pas valide !");
+    }
 }
 
+const calculInteret = () => {
+    solde += solde * TAUX_INTERET;
+    console.log(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${solde} euros.`);
+}
 
 // Dépôt
-operationDepot(depot,historiqueOperations)
+operationSolde(depot, historiqueOperations);
 
 // Retrait
-
-if(solde - retrait > 0){
-    operationRetrait(retrait,historiqueOperations)
-} else {
-    console.log(
-        "Solde insuffisant pour effectuer ce retrait."
-    )
-}
+operationSolde(retrait, historiqueOperations);
 
 // Calcul des intérêts
+calculInteret();
 
-calculInteret(solde)
+// Simulation de plusieurs opérations
+let nouveauDepot = 500;
 
-//Simulation de plusieurs opérations
-
-let nouveauDepot = 500
-
-operationDepot(nouveauDepot,historiqueOperations)
-
-let nouveauRetrait = 800
-
-if(solde - nouveauRetrait > 0){
-    operationRetrait(nouveauRetrait,historiqueOperations)
-
-} else {
-    console.log("Solde insuffisant pour effectuer ce retrait.")
-}
-
-calculInteret(solde)
+operationSolde(nouveauDepot, historiqueOperations);
+calculInteret();
