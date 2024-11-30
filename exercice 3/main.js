9aa83b31ee2edbf7a25612b7ff1f1eb907bbafb7
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-30 14:10:51 +0100

    update form handling

--- exercice 3/main.js
+++ exercice 3/main.js
@@ -3,20 +3,19 @@ import Logger from "Logger";
 
 document.addEventListener("DOMContentLoaded", () => {
     new Logger(document.querySelector("output"));
-
     const compte = new CompteBancaire(1000);
-    console.log(`Initial balance: ${compte.balance} euros.`);
 
-    compte.performTransaction(200);
-    compte.performTransaction(-150);
-    compte.calculateInterest();
+    const transactionForm = document.querySelector("#transaction-form");
+    const transactionInput = document.querySelector("#transaction-amount");
+    const calculateInterestBtn = document.querySelector("#calculate-interest");
 
-    // console.table(compte.history);
+    transactionForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        compte.performTransaction(transactionInput.value);
+        transactionInput.value = "";
+    });
 
-    compte.performTransaction(500);
-    compte.performTransaction(-800);
-    compte.calculateInterest();
-
-    console.log("Final transaction history:");
-    console.table(compte.history);
+    calculateInterestBtn.addEventListener("click", () => {
+        compte.calculateInterest();
+    });
 });
